df132782bfaaf062b4ec18452722511d8c3bf46a
gregdeli <gregorydel@hotmail.gr>
2023-01-16 18:43:13 +0200

    f

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -11,7 +11,16 @@
 /* global definitions */
 
 /* definition and implementation of process descriptor and queue(s) */
-
+// struct pou antiprosopeuei ena process
+struct Work
+{
+    int number; //o arithmos tou executable workN 
+    int priority;
+    pid_t pid;
+    double time; //elapsed time 
+    double start_time;
+    char *command; 
+};
 /* global variables and data structures */
 
 /* signal handler(s) */
@@ -32,10 +41,12 @@ int main(int argc,char **argv)
     // read input arguments
     char *algorithm = argv[1];
     char *input_file;
+    // an den uparxei quantum
     if (argc == 3)
     {
         char *input_file = argv[2];
     }
+    // an uparxei quantum
     int quantum = 0;
     if (argc == 4)
     {
@@ -46,6 +57,58 @@ int main(int argc,char **argv)
     }
 
 	/* read input file - populate queue */
+
+    int line_count = 0; //arithmos ton grammwn sto input_file
+    char row;
+    char getname[5];
+
+    int priority;
+
+    FILE *file1 = fopen(input_file, "r");
+    if (file1 == NULL)
+    {
+        printf("Could not open file\n");
+        return -1;
+    }
+
+    // metrame ton arithmo twn grammwm tou input_file
+    for (row = getc(file1); row != EOF; row = getc(file1))
+        if (row == '\n') // Increment line_count if this character is newline
+            line_count++;
+
+    fclose(file1);
+
+    // dimiourgw pinaka apo structs typou Work
+    struct Work works[line_count];
+
+    // open the file
+    FILE *file = fopen(input_file, "r");
+    if (file == NULL)
+    {
+        printf("Could not open file\n");
+        return -1;
+    }
+
+    int n, p;
+
+    char line[50];
+    FILE *fp = fopen(input_file, "r");
+    for (int i = 0; i < 7; i++)
+    {
+        fgets(line, sizeof(line), fp);
+        sscanf(line, "../work/work%d", &n);
+        sprintf(works[i].command[i], "../work/work%d", n);
+        works[i].number = n;
+    }
+    fclose(fp);
+
+    // read the file
+    int j = 0;
+    while (fscanf(file1, "%s %d", works[j].command[j], &priority) != EOF)
+    {
+        works[j].priority = priority;
+        j++;
+    }
 
 	/* call selected scheduling policy */
 
