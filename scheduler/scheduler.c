8b4984ee0536a95a1852f67236a2acfad4606f4c
greg <77391047+gregdeli@users.noreply.github.com>
2023-01-25 17:35:03 +0200

    A

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -8,51 +8,61 @@
 #include <time.h>
 #include <signal.h>
 #include <string.h>
+#include <errno.h>
+#include <stdbool.h>
 
 /* global definitions */
 #define MAX_QUEUE_SIZE 10
-#define MAX_LEN_COMMAND 15 //max length of an executable string px ../work/work7
+#define MAX_LEN_COMMAND 15 // max length of an executable string px ../work/work7
 
 /* definition and implementation of process descriptor and queue(s) */
 
-//struct pou antiprosopeuei ena process
+// struct pou antiprosopeuei ena process
 struct Work
 {
-    int number; //o arithmos tou executable workN 
+    int number; // o arithmos tou executable workN
     int priority;
     pid_t pid;
-    double time; //elapsed time 
-    //double start_time;
-    char command[MAX_LEN_COMMAND]; 
-    struct Work *next; //pointer gia to queue
+    double time; // elapsed time
+    // double start_time;
+    char command[MAX_LEN_COMMAND];
+    struct Work *next; // pointer gia to queue
     struct Work *prev;
+    int status;
+    bool exited;
 };
 
 /* global variables and data structures */
 
-//queue with doubly linked list 
+struct Work *current_process; //global pointer se Work gia xrisi sto catch_sigchld()
+struct timeval start, end; //gia ne metrisw ton xrono ektelesis kathe diergasias
+
+// queue with doubly linked list
 struct WorkQueue
 {
     struct Work *head;
     struct Work *tail;
 };
 
-void init_queue(struct WorkQueue *q) {
+
+void init_queue(struct WorkQueue *q)
+{
     q->head = NULL;
     q->tail = NULL;
 }
 
-void enqueue(struct WorkQueue *q, struct Work *new_process) {
+void enqueue(struct WorkQueue *q, struct Work *new_process)
+{
 
     new_process->prev = NULL;
     new_process->next = NULL;
 
-    if (q->tail == NULL) 
+    if (q->tail == NULL)
     {
         q->head = new_process;
         q->tail = new_process;
     }
-    else 
+    else
     {
         q->tail->next = new_process;
         new_process->prev = q->tail;
@@ -60,26 +70,27 @@ void enqueue(struct WorkQueue *q, struct Work *new_process) {
     }
 }
 
-struct Work* dequeue(struct WorkQueue *q) {
-    if (q->head == NULL) 
+struct Work *dequeue(struct WorkQueue *q)
+{
+    if (q->head == NULL)
     {
         return NULL;
     }
-    
+
     struct Work *first_process = q->head;
     q->head = first_process->next;
 
-    //kanoniki katastasi
-    //an meta tin afairesi tou prwtou process apo tin lista/queue o deiktis q->head deixnei kapou tote thetw ton deikti 
-    //prev tou kainouriou head process ws null
-    if (q->head != NULL) 
+    // kanoniki katastasi
+    // an meta tin afairesi tou prwtou process apo tin lista/queue o deiktis q->head deixnei kapou tote thetw ton deikti
+    // prev tou kainouriou head process ws null
+    if (q->head != NULL)
     {
         q->head->prev = NULL;
     }
 
-    //an meta tin afairesi tou prwtou process apo tin lista o deiktis q->head ginei NULL simainei oti ekana dequeue to 
-    //telautaio process opote to q->tail edeixne ekei pou edeixne arxika kai o q->head opote prepei kai autos na ginei NULL 
-    if (q->head == NULL) 
+    // an meta tin afairesi tou prwtou process apo tin lista o deiktis q->head ginei NULL simainei oti ekana dequeue to
+    // telautaio process opote to q->tail edeixne ekei pou edeixne arxika kai o q->head opote prepei kai autos na ginei NULL
+    if (q->head == NULL)
     {
         q->tail = NULL;
     }
@@ -89,6 +100,19 @@ struct Work* dequeue(struct WorkQueue *q) {
 
 /* signal handler(s) */
 
+void catch_sigchld(int sig)
+{
+    waitpid(current_process->pid, &current_process->status, WNOHANG);
+    if(WIFEXITED(current_process->status))
+    {
+        current_process->exited = true;
+        gettimeofday(&end, NULL);
+        current_process->time = current_process->time + ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0);
+        
+    }
+    
+}
+
 /* implementation of the scheduling policies, etc. batch(), rr() etc. */
 
 void FCFS(int process_count, struct WorkQueue q, struct Work works[])
@@ -96,13 +120,13 @@ void FCFS(int process_count, struct WorkQueue q, struct Work works[])
     for (int i = 0; i < process_count; i++)
     {
         struct Work *first_work = dequeue(&q);
-        
+
         int pid = fork();
         if (pid == 0)
         {
             // This is the child process
             char *args[] = {first_work->command, NULL};
-            execvp(first_work->command, args);   
+            execvp(first_work->command, args);
             exit(0);
         }
         else if (pid > 0)
@@ -118,83 +142,146 @@ void FCFS(int process_count, struct WorkQueue q, struct Work works[])
     }
 }
 
-void RR(struct WorkQueue *q, int quantum)
+void SJF(int process_count, struct WorkQueue q, struct Work works[])
 {
-    struct timespec sleep_time;
-    sleep_time.tv_sec = 0;
-    sleep_time.tv_nsec = quantum;
-    struct Work current_process;
-    while(q->head != NULL)
+    struct Work temp[process_count];
+    for (int i = 1; i < process_count; i++)
     {
-        struct Work *current_process = dequeue(q);
-        struct timeval start, end;
-        gettimeofday(&start, NULL);
-        int status;
-        if(current_process->pid == -3)
+        temp[0].priority = works[i].priority;
+        temp[0].pid = works[i].pid;
+        temp[0].time = works[i].time;
+        temp[0].number = works[i].number;
+        strcpy(temp[0].command, works[i].command);
+        temp[0].next = works[i].next;
+        temp[0].prev = works[i].prev;
+        int j = i - 1;
+
+        while (temp[0].priority < works[j].priority && j >= 0)
         {
-            current_process->pid = fork();
+            works[j + 1].priority = works[j].priority;
+            works[j + 1].pid = works[j].pid;
+            works[j + 1].time = works[j].time;
+            works[j + 1].number = works[j].number;
+            strcpy(works[j + 1].command, works[j].command);
+            works[j + 1].next = works[j].next;
+            works[j + 1].prev = works[j].prev;
+            --j;
         }
-        else
+        works[j + 1].priority = temp[0].priority;
+        works[j + 1].pid = temp[0].pid;
+        works[j + 1].time = temp[0].time;
+        works[j + 1].number = temp[0].number;
+        strcpy(works[j + 1].command, temp[0].command);
+        works[j + 1].next = temp[0].next;
+        works[j + 1].prev = temp[0].prev;
+    }
+
+    for (int i = 0; i < process_count; i++)
+    {
+        enqueue(&q, &works[i]);
+    }
+
+    for (int i = 0; i < process_count; i++)
+    {
+        struct Work *first_work = dequeue(&q);
+
+        int pid = fork();
+        if (pid == 0)
         {
-            kill(current_process->pid, SIGCONT);
-        }
-        if (current_process->pid == 0)
-        {
-            //child process
-            char *args[] = {current_process->command, NULL};
-            execvp(current_process->command, args);  
+            // This is the child process
+            char *args[] = {first_work->command, NULL};
+            execvp(first_work->command, args);
             exit(0);
         }
+        else if (pid > 0)
+        {
+            // This is the parent process
+            struct timeval start, end;
+            gettimeofday(&start, NULL);
+            waitpid(pid, NULL, 0);
+            gettimeofday(&end, NULL);
+            works[i].pid = pid;
+            works[i].time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+        }
+    }
+}
+
+void RR(int quantum, struct WorkQueue *q)
+{
+    struct timespec sleep_time;
+    sleep_time.tv_sec = quantum/1000;
+    sleep_time.tv_nsec = 0;
+
+    signal(SIGCHLD,catch_sigchld); //kanw register tin catch_sigchld function ws handler for the SIGCHLD signal
+
+    while (q->head != NULL)
+    {
+        //int ret_val = 0;
+        current_process = dequeue(q);
+        gettimeofday(&start, NULL); //start timer
+
+        if(WIFSTOPPED(current_process->status)){
+            kill(current_process->pid, SIGCONT);
+        }
+        else if(current_process->pid == -1){
+            current_process->pid = fork();
+        }
+
+        if (current_process->pid == 0)
+        {
+            // child process
+            char *args[] = {current_process->command, NULL};
+            execvp(current_process->command, args);
+            exit(0);
+        }
+
         else
         {
-            //parent process
-            //sleep(quantum);
-            nanosleep(&sleep_time,NULL);
-            
-            waitpid(current_process->pid, &status, 0);
-            kill(current_process->pid, SIGSTOP);
-            if(WIFEXITED(status))
-            {
-                 gettimeofday(&end, NULL);
-                 current_process->time = current_process->time + (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
-            }
-            else
-            {
+            // parent process
+            int ret_val = nanosleep(&sleep_time, NULL); // sleep gia xrono iso me to quantum
+            if(current_process->exited && ret_val == -1){
                 gettimeofday(&end, NULL);
-                current_process->time = current_process->time + (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
-                enqueue(q, current_process);
+                current_process->time = current_process->time + ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0);
+                continue;
+            }
+            else{
+                kill(current_process->pid, SIGSTOP);
+                waitpid(current_process->pid, &current_process->status, WSTOPPED);
+                enqueue(q,current_process);
+                gettimeofday(&end, NULL);
+                current_process->time = current_process->time + ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0);
             }
             
         }
     }
 }
 
-void PRIO(){
-    //algorithmos tou jj edw 
+void PRIO()
+{
+    // algorithmos tou jj edw
 }
 
-
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
-	/* local variables */
+    /* local variables */
 
-	/* parse input arguments (policy, quantum (if required), input filename */
+    /* parse input arguments (policy, quantum (if required), input filename */
 
-	//check if the number of arguments is correct
+    // check if the number of arguments is correct
     if (argc != 3 && argc != 4)
     {
         printf("Invalid number of arguments.\nUsage: ./scheduler <algorithm> [quantum] <input_file>\n");
         return -1;
     }
-    //read input arguments
+    // read input arguments
     char *algorithm = argv[1];
     char *input_file;
-    //an den uparxei quantum
+    // an den uparxei quantum
     if (argc == 3)
     {
         input_file = argv[2];
     }
-    //an uparxei quantum
+    // an uparxei quantum
     int quantum = 0;
     if (argc == 4)
     {
@@ -203,8 +290,8 @@ int main(int argc,char **argv)
         quantum = atoi(argv[2]);
     }
 
-	/* read input file - populate queue */
-    
+    /* read input file - populate queue */
+
     int count = 0;
     char row;
     FILE *file = fopen(input_file, "r");
@@ -214,43 +301,47 @@ int main(int argc,char **argv)
         return -1;
     }
 
-    //metrame ton arithmo twn grammwn tou input_file
+    // metrame ton arithmo twn grammwn tou input_file
     for (row = getc(file); row != EOF; row = getc(file))
         if (row == '\n') // Increment count if this character is newline
             count++;
 
-    rewind(file); //pigainoume ton file pointer stin arxi tou arxeiou
+    rewind(file); // pigainoume ton file pointer stin arxi tou arxeiou
 
     char line[50];
     struct Work processes[count];
-    //init work structs 
-    for(int i=0; i<count; i++){
+
+    // init work 
+    for (int i = 0; i < count; i++)
+    {
         processes[i].time = 0;
-        processes[i].pid = -3;
+        processes[i].pid = -1;
+        processes[i].status = -1;
+        processes[i].exited = false;
     }
 
     char *token;
 
     int i = 0;
-    int n; //apo to workn
+    int n; // apo to workn
     while (fgets(line, 50, file) != NULL)
     {
         token = strtok(line, "\t");
         strcpy(processes[i].command, token);
-        sscanf(token, "../work/work%d", &n); //diavazw ton arithmo tis diergasias
+        sscanf(token, "../work/work%d", &n); // diavazw ton arithmo tis diergasias
         processes[i].number = n;
         token = strtok(NULL, "\t");
         processes[i].priority = atoi(token);
         i++;
     }
-    
+
     fclose(file);
 
     //initiate queue
     struct WorkQueue q;
     init_queue(&q);
 
-	/* call selected scheduling policy */
+    /* call selected scheduling policy */
 
     if (strcmp(algorithm, "BATCH") == 0)
     {
@@ -262,8 +353,13 @@ int main(int argc,char **argv)
         FCFS(count, q, processes);
         printf("\n\n# scheduler %s %s\n\n", algorithm, input_file);
     }
+    else if (strcmp(algorithm, "SJF") == 0)
+    {
 
-    if (strcmp(algorithm, "RR") == 0)
+        SJF(count, q, processes);
+        printf("\n\n# scheduler %s %s\n\n", algorithm, input_file);
+    }
+    else if (strcmp(algorithm, "RR") == 0)
     {
         if (quantum == 0)
         {
@@ -274,34 +370,38 @@ int main(int argc,char **argv)
         {
             enqueue(&q, &processes[i]);
         }
-        RR(&q, quantum);
+        RR(quantum, &q);
         printf("\n\n# scheduler %s %d %s\n\n", algorithm, quantum, input_file);
     }
 
-    if (strcmp(algorithm, "PRIO") == 0)
+    else if (strcmp(algorithm, "PRIO") == 0)
     {
         if (quantum == 0)
         {
             printf("Error: Quantum not specified for RR algorithm\n");
             return 1;
         }
-        //klisi synartisis jj edw
+        // klisi synartisis jj edw
 
         printf("\n\n# scheduler %s %d %s\n\n", algorithm, quantum, input_file);
     }
+    else
+    {
+        printf("Error: Unknown algorithm %s\n", algorithm);
+        return 1;
+    }
 
-	/* print information and statistics */
-    
-    double workload = processes[0].time;
+    /* print information and statistics */
+
+    double workload = 0;
     for (int i = 0; i < count; i++)
     {
-        printf("Work: %d, Priority: %d, PID: %d, Elapsed Time: %.3lf, Workload Time: %.3lf\n", 
-                processes[i].number, processes[i].priority, processes[i].pid, 
-                processes[i].time, workload);
-
         workload = workload + (processes[i].time);
+        printf("Work: %d, Priority: %d, PID: %d, Elapsed Time: %.3lf, Workload Time: %.3lf\n",
+               processes[i].number, processes[i].priority, processes[i].pid,
+               processes[i].time, workload);
     }
     printf("\nWORKLOAD TIME: %.3lf\n\n", workload);
 
-	return 0;
+    return 0;
 }
